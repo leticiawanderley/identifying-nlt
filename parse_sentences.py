f22a456ec9ba50292a08ebaf81f7e28924e02fd7
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-05-06 15:06:02 -0600

    Update conjunction parsing

--- parse_sentences.py
+++ parse_sentences.py
@@ -1,52 +1,57 @@
 import pandas as pd
 import spacy
 
-models = {
-  'en': spacy.load("en_core_web_md"),
-  'es': spacy.load("es_core_news_md")
-}
+CONJ = 'CONJ'
+CCONJ = 'CCONJ'
+SPANISH = 'es'
 
 
-def pre_process_data(filename, selected_columns, new_column_names):
+def pre_process_data(filename, new_column_names, selected_columns=None):
     """Create pandas dataframe with selected columns."""
     df = pd.read_csv(filename)
-    df = df[selected_columns]
+    if selected_columns:
+        df = df[selected_columns]
     df.columns = new_column_names
     return df
 
 
-def pos_tag(df, languages_columns):
+def pos_tag(models, df, languages_columns):
     """Add part-of-speech tags columns to dataframe."""
     for lang in languages_columns.keys():
         df[lang + '_pos'] = df.apply(lambda x:
-                                     tag_sentences(x[languages_columns[lang]],
+                                     tag_sentences(models,
+                                                   x[languages_columns[lang]],
                                                    lang), axis=1)
     return df
 
 
-def tag_sentences(sentence, language):
+def tag_sentences(models, sentence, language):
     """Part-of-speeh tag dataframe sentence."""
     tags = ''
     if type(sentence) == str:
         nlp = models[language]
         doc = nlp(sentence)
         for token in doc:
-            tags += token.pos_ + ' '
+            pos = token.pos_
+            if token.pos_ == CONJ:
+                pos = CCONJ if language == SPANISH else pos
+            tags += pos + ' '
     return tags
 
 
-def main():
-    selected_columns = ['english', 'spanish']
+def main(models, input_filename, output_filename):
     new_column_names = ['english', 'spanish']
-    filename = 'data/dataset_sentences.csv'
-    df = pre_process_data(filename, selected_columns, new_column_names)
-
+    df = pre_process_data(input_filename, new_column_names)
     languages_columns = {'en': 'english', 'es': 'spanish'}
-    df = pos_tag(df, languages_columns)
-
-    results_filename = 'data/tagged_sentences_dataset_sentences.csv'
-    df.to_csv(results_filename, index=True)
+    df = pos_tag(models, df, languages_columns)
+    df.to_csv(output_filename, index=True)
 
 
 if __name__ == "__main__":
-    main()
+    input_filename = 'data/1000sents.csv'
+    output_filename = 'data/tagged_sentences_1000sents.csv'
+    models = {
+        'en': spacy.load("en_core_web_md"),
+        'es': spacy.load("es_core_news_md")
+    }
+    main(models, input_filename, output_filename)
