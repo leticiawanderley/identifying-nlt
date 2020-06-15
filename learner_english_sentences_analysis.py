794568beb5e24ba928a427b70bb15120c02d579e
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-06-15 15:23:41 -0600

    Analyse performance using UD tags

--- learner_english_sentences_analysis.py
+++ learner_english_sentences_analysis.py
@@ -15,15 +15,22 @@ def evaluate_models(filename, fields):
 
 
 fields = ['sentence', 'correct', 'tags', 'trigram']
+fields_poss = ['sentence', 'correct', 'tags', 'poss']
 evaluate_models('data/results_learner_english_unsmoothed.csv',
                 fields)
 evaluate_models('data/results_learner_english_trigrams_unsmoothed.csv',
                 fields)
+evaluate_models('data/results_learner_english_unsmoothed_poss.csv',
+                fields_poss)
 evaluate_models('data/results_learner_english_laplace.csv',
                 fields)
 evaluate_models('data/results_learner_english_trigrams_laplace.csv',
                 fields)
+evaluate_models('data/results_learner_english_laplace_poss.csv',
+                fields_poss)
 evaluate_models('data/results_learner_english_interpolation.csv',
                 fields)
 evaluate_models('data/results_learner_english_trigrams_interpolation.csv',
                 fields)
+evaluate_models('data/results_learner_english_interpolation_poss.csv',
+                fields_poss)
