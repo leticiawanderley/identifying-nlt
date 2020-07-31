dcc7a7719069581a4015143ea3fda780bb7b7c18
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-07-31 15:13:56 -0600

    Refactor and extract RNN preprocessing functions

---
+++ rnn_data_preprocessing.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import torch
+
+
+def read_data(filenames, columns):
+    data = {}
+    for column in columns:
+        data[column] = []
+    for filename in filenames:
+        df = pd.read_csv(filename)
+        for c in columns:
+            data[c] += df[c].to_list()
+    return data, list(data.keys())
+
+
+def get_all_tags(languages_tag_files):
+    all_tags = {}
+    for tag_file in languages_tag_files:
+        df = pd.read_csv(tag_file, index_col=0)
+        for tag in df['ngram'].to_list():
+            if tag not in all_tags:
+                all_tags[tag] = True
+    return sorted(all_tags.keys())
+
+
+def tag_to_index(tag, all_tags):
+    return all_tags.index(tag)
+
+
+def tag_to_tensor(tag, n_tags, all_tags):
+    tensor = torch.zeros(1, n_tags)
+    tensor[0][tag_to_index(tag, all_tags)] = 1
+    return tensor
+
+
+def sentence_to_tensor(sentence, n_tags, all_tags):
+    sentence = sentence.split()
+    tensor = torch.zeros(len(sentence), 1, n_tags)
+    for li, tag in enumerate(sentence):
+        tensor[li][0][tag_to_index(tag, all_tags)] = 1
+    return tensor
