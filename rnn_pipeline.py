c6c26658f27fb1b516963978027ed2c1da5a0d0e
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-08-05 11:50:23 -0600

    Save RNN model to a file and reuse it to test

--- rnn_pipeline.py
+++ rnn_pipeline.py
@@ -9,38 +9,42 @@ from rnn_visualization_functions import confusion_matrix, losses
 from utils import time_since
 
 
-def main():
-    categories = ['en_ud', 'es_ud']
-    raw_data = read_data(
-        ['data/training data/europarl/english_spanish_10_tagged.csv',
-         'data/training data/europarl/english_spanish_11_tagged.csv',
-         'data/training data/europarl/english_spanish_12_tagged.csv',
-         'data/training data/europarl/english_spanish_13_tagged.csv',
-         'data/training data/europarl/english_spanish_14_tagged.csv',
-         'data/training data/europarl/english_spanish_15_tagged.csv',
-         'data/training data/europarl/english_spanish_16_tagged.csv',
-         'data/training data/europarl/english_spanish_17_tagged.csv',
-         'data/training data/europarl/english_spanish_18_tagged.csv',
-         'data/training data/europarl/english_spanish_19_tagged.csv',
-         'data/training data/europarl/english_spanish_20_tagged.csv'],
-        categories)
-
+def main(train_new_model=True):
     all_tags = get_all_tags(['data/training data/es_ud_0_vocab.csv',
                              'data/training data/en_ud_0_vocab.csv'])
-    data = Data(raw_data, all_tags)
+    categories = ['en_ud', 'es_ud']
     n_hidden = 256
-    rnn = RNN(len(all_tags), n_hidden, len(categories))
-    n_iters = data.size
-    print_every = 5000
-    plot_every = 1000
-    all_losses = run_training(rnn, data, categories,
-                              n_iters, print_every, plot_every)
-    losses(all_losses, 'all_losses.png')
+    saved_model_path = './saved_model.pth'
+    if train_new_model:
+        training_data = read_data(
+            ['data/training data/europarl/english_spanish_10_tagged.csv',
+             'data/training data/europarl/english_spanish_11_tagged.csv',
+             'data/training data/europarl/english_spanish_12_tagged.csv',
+             'data/training data/europarl/english_spanish_13_tagged.csv',
+             'data/training data/europarl/english_spanish_14_tagged.csv',
+             'data/training data/europarl/english_spanish_15_tagged.csv',
+             'data/training data/europarl/english_spanish_16_tagged.csv',
+             'data/training data/europarl/english_spanish_17_tagged.csv',
+             'data/training data/europarl/english_spanish_18_tagged.csv',
+             'data/training data/europarl/english_spanish_19_tagged.csv',
+             'data/training data/europarl/english_spanish_20_tagged.csv'],
+            categories)
+        data = Data(training_data, all_tags)
+        rnn = RNN(len(all_tags), n_hidden, len(categories))
+        n_iters = data.size
+        print_every = 5000
+        plot_every = 1000
+        all_losses = run_training(rnn, data, categories,
+                                  n_iters, print_every, plot_every)
+        losses(all_losses, 'all_losses.png')
     test_data = read_data(
         ['data/training data/tagged_sentences_1000sents.csv'],
         categories)
     data = Data(test_data, all_tags)
-    confusion = build_confusion_data(rnn, categories, data)
+    saved_rnn = RNN(len(all_tags), n_hidden, len(categories))
+    saved_rnn.load_state_dict(torch.load(saved_model_path))
+    saved_rnn.eval()
+    confusion = build_confusion_data(saved_rnn, categories, data)
     confusion_matrix(confusion, categories, 'confusion_matrix.png')
 
 
@@ -70,6 +74,7 @@ def run_training(rnn, data, categories, n_iters,
         if iter % plot_every == 0:
             all_losses.append(current_loss / plot_every)
             current_loss = 0
+    torch.save(rnn.state_dict(), './saved_model.pth')
     return all_losses
 
 
