18bcc0bc2e1b6ddf2cad865d374bfaace1fb18d9
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-08-06 11:06:34 -0600

    Add Chinese Global Voices dataset

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - [1000 sentences](https://www.kaggle.com/bryanpark/parallelsents)
 - [Language Identification dataset](https://www.kaggle.com/zarajamshaid/language-identification-datasst)
 - [European Parliament Proceedings Parallel Corpus 1996-2011](https://www.kaggle.com/djonafegnem/europarl-parallel-corpus-19962011)
+- [Global Voices Parallel Corpus 2018Q4 (zhs-en)](http://casmacat.eu/corpus/global-voices.html)
 
 
 ### Datasets used to test language models
---
+++ process_chinese_data.py
@@ -0,0 +1,30 @@
+import re
+import pandas as pd
+
+
+def extract_sentences(filename):
+    source_re = r'(?<=<source>)(.*?)(?=<\/source>)'
+    target_re = r'(?<=<target>)(.*?)(?=<\/target>)'
+    data_dict = {'zhs': [], 'en': []}
+    file = open(filename).readlines()
+    for line in file:
+        if '<source>' in line:
+            source_match = re.search(source_re, line, re.MULTILINE)
+            data_dict['zhs'].append(source_match.groups()[0])
+        elif '<target>' in line:
+            target_match = re.search(target_re, line, re.MULTILINE)
+            data_dict['en'].append(target_match.groups()[0])
+    return data_dict
+
+
+def main(input_filename, output_filename):
+    data_dict = extract_sentences(input_filename)
+    df = pd.DataFrame.from_dict(data_dict)
+    df.to_csv(output_filename)
+
+
+if __name__ == "__main__":
+    input_filename = 'data/training data/globalvoices.zhs-en.xliff'
+    output_filename = ('data/training data/'
+                       'globalvoices_sentences.csv')
+    main(input_filename, output_filename)
