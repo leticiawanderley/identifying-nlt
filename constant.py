2a560b5b05c72f893fb78f6e0efb89b3e081421b
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-08-06 13:23:14 -0600

    Update constants with new vocab locations

--- constant.py
+++ constant.py
@@ -10,20 +10,28 @@ CCONJ = 'CCONJ'
 SPANISH = 'es'
 CHINESE = 'zhs'
 PENN_NGRAMS_FILES = {
-    'en': {0: 'data/training data/en_penn_0_vocab.csv',
-           1: 'data/training data/en_penn_1_vocab.csv',
-           2: 'data/training data/en_penn_2_vocab.csv'},
-    'es': {0: 'data/training data/es_penn_0_vocab.csv',
-           1: 'data/training data/es_penn_1_vocab.csv',
-           2: 'data/training data/es_penn_2_vocab.csv'}
+    'en': {0: 'data/training data/europarl_vocabs/en_penn_0_vocab.csv',
+           1: 'data/training data/europarl_vocabs/en_penn_1_vocab.csv',
+           2: 'data/training data/europarl_vocabs/en_penn_2_vocab.csv'},
+    'es': {0: 'data/training data/europarl_vocabs/es_penn_0_vocab.csv',
+           1: 'data/training data/europarl_vocabs/es_penn_1_vocab.csv',
+           2: 'data/training data/europarl_vocabs/es_penn_2_vocab.csv'}
 }
 UD_NGRAMS_FILES = {
-    'en': {0: 'data/training data/en_ud_0_vocab.csv',
-           1: 'data/training data/en_ud_1_vocab.csv',
-           2: 'data/training data/en_ud_2_vocab.csv'},
-    'es': {0: 'data/training data/es_ud_0_vocab.csv',
-           1: 'data/training data/es_ud_1_vocab.csv',
-           2: 'data/training data/es_ud_2_vocab.csv'}
+    'en': {0: 'data/training data/europarl_vocabs/en_ud_0_vocab.csv',
+           1: 'data/training data/europarl_vocabs/en_ud_1_vocab.csv',
+           2: 'data/training data/europarl_vocabs/en_ud_2_vocab.csv'},
+    'es': {0: 'data/training data/europarl_vocabs/es_ud_0_vocab.csv',
+           1: 'data/training data/europarl_vocabs/es_ud_1_vocab.csv',
+           2: 'data/training data/europarl_vocabs/es_ud_2_vocab.csv'}
+}
+UD_NGRAMS_FILES_GV = {
+    'en': {0: 'data/training data/globalvoices_vocabs/en_ud_0_vocab.csv',
+           1: 'data/training data/globalvoices_vocabs/en_ud_1_vocab.csv',
+           2: 'data/training data/globalvoices_vocabs/en_ud_2_vocab.csv'},
+    'zhs': {0: 'data/training data/globalvoices_vocabs/zhs_ud_0_vocab.csv',
+            1: 'data/training data/globalvoices_vocabs/zhs_ud_1_vocab.csv',
+            2: 'data/training data/globalvoices_vocabs/zhs_ud_2_vocab.csv'}
 }
 LEARNER_ENGLISH_FIELDS = ['sentence', 'correct', 'error_type', 'penn',
                           'penn_trigram', 'ud', 'ud_trigram']
