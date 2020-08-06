fbe48b22b0dcc6e25e02c80f80bd2370dbc85661
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-08-06 16:58:39 -0600

    Test annotated FCE dataset with RNN model

--- rnn_pipeline.py
+++ rnn_pipeline.py
@@ -1,6 +1,7 @@
 import time
 import torch
 import torch.nn as nn
+import pandas as pd
 
 from rnn import RNN
 from rnn_data_preprocessing import get_all_tags, read_data
@@ -10,24 +11,16 @@ from utils import time_since
 
 
 def main(train_new_model=True):
-    all_tags = get_all_tags(['data/training data/es_ud_0_vocab.csv',
-                             'data/training data/en_ud_0_vocab.csv'])
-    categories = ['en_ud', 'es_ud']
+    all_tags = get_all_tags(['data/training data/globalvoices_vocabs/' +
+                             'zhs_ud_0_vocab.csv',
+                             'data/training data/globalvoices_vocabs/' +
+                             'en_ud_0_vocab.csv'])
+    categories = ['en_ud', 'zhs_ud']
     n_hidden = 256
-    saved_model_path = './saved_model.pth'
+    saved_model_path = './saved_model_zhs_en_1.pth'
     if train_new_model:
         training_data = read_data(
-            ['data/training data/europarl/english_spanish_10_tagged.csv',
-             'data/training data/europarl/english_spanish_11_tagged.csv',
-             'data/training data/europarl/english_spanish_12_tagged.csv',
-             'data/training data/europarl/english_spanish_13_tagged.csv',
-             'data/training data/europarl/english_spanish_14_tagged.csv',
-             'data/training data/europarl/english_spanish_15_tagged.csv',
-             'data/training data/europarl/english_spanish_16_tagged.csv',
-             'data/training data/europarl/english_spanish_17_tagged.csv',
-             'data/training data/europarl/english_spanish_18_tagged.csv',
-             'data/training data/europarl/english_spanish_19_tagged.csv',
-             'data/training data/europarl/english_spanish_20_tagged.csv'],
+            ['data/training data/tagged_globalvoices_sentences.csv'],
             categories)
         data = Data(training_data, all_tags)
         rnn = RNN(len(all_tags), n_hidden, len(categories))
@@ -35,23 +28,25 @@ def main(train_new_model=True):
         print_every = 5000
         plot_every = 1000
         all_losses = run_training(rnn, data, categories,
-                                  n_iters, print_every, plot_every)
-        losses(all_losses, 'all_losses.png')
+                                  n_iters, print_every, plot_every,
+                                  saved_model_path)
+        losses(all_losses, 'all_losses_zhs_es_1.png')
     test_data = read_data(
-        ['data/training data/tagged_sentences_1000sents.csv'],
-        categories)
+        ['data/testing data/annotated_FCE/chinese_annotated_errors.csv'],
+        ['incorrect_trigram_ud'])
+    test_data['zhs_ud'] = test_data.pop('incorrect_trigram_ud')
     data = Data(test_data, all_tags)
     saved_rnn = RNN(len(all_tags), n_hidden, len(categories))
     saved_rnn.load_state_dict(torch.load(saved_model_path))
     saved_rnn.eval()
     confusion = build_confusion_data(saved_rnn, categories, data)
-    confusion_matrix(confusion, categories, 'confusion_matrix.png')
+    confusion_matrix(confusion, categories, 'confusion_matrix_zhs_en_1.png')
 
 
 def run_training(rnn, data, categories, n_iters,
-                 print_every, plot_every):
+                 print_every, plot_every, output_model):
     start = time.time()
-    learning_rate = 0.0002
+    learning_rate = 0.0001
     criterion = nn.NLLLoss()
     current_loss = 0
     all_losses = []
@@ -75,11 +70,12 @@ def run_training(rnn, data, categories, n_iters,
         if iteration % plot_every == 0:
             all_losses.append(current_loss / plot_every)
             current_loss = 0
-    torch.save(rnn.state_dict(), './saved_model.pth')
+    torch.save(rnn.state_dict(), output_model)
     return all_losses
 
 
 def build_confusion_data(rnn, categories, data):
+    results_dict = {'sequence': [], 'guess':[]}
     n_categories = len(categories)
     # Keep track of correct guesses in a confusion matrix
     confusion = torch.zeros(n_categories, n_categories)
@@ -93,11 +89,16 @@ def build_confusion_data(rnn, categories, data):
         guess, guess_i = category_from_output(output, categories)
         category_i = categories.index(category)
         confusion[category_i][guess_i] += 1
+        results_dict['sequence'].append(sequence)
+        results_dict['guess'].append(guess)
 
+    df = pd.DataFrame.from_dict(results_dict)
+    df.to_csv('rnn_results.csv')
     # Normalize by dividing every row by its sum
     for i in range(n_categories):
         confusion[i] = confusion[i] / confusion[i].sum()
+    print(confusion)
     return confusion
 
 
-main()
+main(False)
