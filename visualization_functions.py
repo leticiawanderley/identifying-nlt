e884b1b721b7e3fb8b0cd4639bc50c9b53612bd6
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-08-11 13:34:17 -0600

    Label confusion matrix axis

--- visualization_functions.py
+++ visualization_functions.py
@@ -12,16 +12,19 @@ def losses(all_losses, output_filename):
 def confusion_matrix(confusion, all_categories, output_filename):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    cax = ax.matshow(confusion.numpy())
+    cax = ax.matshow(confusion)
     fig.colorbar(cax)
 
     for (i, j), z in np.ndenumerate(confusion):
         ax.text(j, i, '{:0.4f}'.format(z), ha='center', va='center')
 
-    ax.set_xticklabels([''] + all_categories, rotation=90)
+    ax.set_xticklabels([''] + all_categories)
     ax.set_yticklabels([''] + all_categories)
 
     ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
     ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
 
     fig.savefig(output_filename)
