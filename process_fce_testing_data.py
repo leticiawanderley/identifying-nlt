fe81daae8b9365c6b3f0f1d4304d0e3716734e86
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-08-11 09:59:37 -0600

    Automatically rename annotated FCE dataset columns

--- process_fce_testing_data.py
+++ process_fce_testing_data.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 
+from constant import ANNOTATED_FCE_FIELDS
+
 
 def merge_datasets(folder, columns, output_filename):
     dataframes = []
@@ -11,6 +13,7 @@ def merge_datasets(folder, columns, output_filename):
             dataframes.append(pd.read_csv(filename)[columns])
     df = pd.concat(dataframes)
     df['Negative transfer?'] = df['Negative transfer?'] == 'Y'
+    df.columns = ANNOTATED_FCE_FIELDS
     df.to_csv(output_filename)
 
 
@@ -19,4 +22,4 @@ merge_datasets('data/testing data/annotated_FCE',
                 'Likely reason for mistake',
                 'correct_sentence', 'correct_trigram_poss',
                 'incorrect_sentence', 'incorrect_trigram_poss'],
-               'chinese_annotated_errors.csv')
+               'data/testing data/annotated_FCE/chinese_annotated_errors.csv')
