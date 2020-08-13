7b4bac5cf429c708e052f19209c364215c39aabb
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-08-13 13:21:27 -0600

    Fix NLI and NLT pipelines call

--- rnn_pipeline.py
+++ rnn_pipeline.py
@@ -84,95 +84,82 @@ def test_nli_rnn(test_dataset, rnn, categories, n_tags, all_tags):
     return output_filename
 
 
-def nli(vocab_datasets, training_datasets, testing_dateset, categories,
+def nli(vocab_datasets, training_datasets, test_dateset, categories,
         n_hidden, saved_model_path, train_new_model=True):
     all_tags = get_all_tags(vocab_datasets)
     if train_new_model:
         training_data = read_data(training_datasets, categories)
-        data = Data(training_data, all_tags)
-        rnn = RNN(len(all_tags), n_hidden, len(categories))
-        n_iters = data.size
-        print_every = 5000
-        plot_every = 1000
         learning_rate = 0.0001
-        all_losses = run_training(rnn, data, categories, learning_rate,
-                                  n_iters, print_every, plot_every,
-                                  saved_model_path)
-        losses(all_losses, 'all_losses_zhs_en_1.png')
-    columns = {True: 'zhs_ud', False: 'en_ud'}
-    test_data = pd.read_csv(testing_dateset)
-    test_data_dict = setup_data(test_data, columns,
-                                'incorrect_trigram_ud', GOLD_LABEL)
-    data = Data(test_data_dict, all_tags)
-    saved_rnn = RNN(len(all_tags), n_hidden, len(categories))
-    saved_rnn.load_state_dict(torch.load(saved_model_path))
-    saved_rnn.eval()
-    results_file = test_annotated_fce(testing_dataset, saved_rnn,
-                                      categories, len(all_tags), all_tags)
+        rnn = train_rnn_model(training_data, categories, all_tags,
+                              learning_rate, saved_model_path,
+                              'all_losses_zhs_en_1.png')
+    else:
+        rnn = RNN(len(all_tags), n_hidden, len(categories))
+        rnn.load_state_dict(torch.load(saved_model_path))
+        rnn.eval()
+    results_file = test_nli_rnn(test_dataset, rnn,
+                                categories, len(all_tags), all_tags)
     confusion_matrix(results_file, GOLD_LABEL, MODEL_LABEL,
                      'confusion_matrix_zhs_en_rnn.png')
 
 
-def predict_nlt(n_hidden, saved_model_path, train_new_model=True):
-    all_tags = get_all_tags(
-                    ['data/training data/incorrect_trigram_ud_0_vocab.csv'])
-    df = pd.read_csv('data/testing data/annotated_FCE/' +
-                     'chinese_annotated_errors.csv')
-    y = df[GOLD_LABEL]
-    x_train, x_test, y_train, y_test = train_test_split(df, y, test_size=0.2)
-    test_data = x_test.copy()
-    columns = {True: True, False: False}
-    categories = list(columns.keys())
-    if train_new_model:
-        data_dict = setup_data(x_train, columns, 'incorrect_trigram_ud',
-                               GOLD_LABEL)
-        data = Data(data_dict, all_tags)
-        rnn = RNN(len(all_tags), n_hidden, len(categories))
-        n_iters = data.size
-        print_every = 50
-        plot_every = 10
-        learning_rate = 0.25
-        all_losses = run_training(rnn, data, categories, learning_rate,
-                                  n_iters, print_every, plot_every,
-                                  saved_model_path)
-        losses(all_losses, 'all_losses_predict_nlt.png')
-    saved_rnn = RNN(len(all_tags), n_hidden, len(categories))
-    saved_rnn.load_state_dict(torch.load(saved_model_path))
-    saved_rnn.eval()
+def test_nlt_rnn(test_data, rnn, categories, n_tags, all_tags):
     nlt = []
     results = []
     for index, row in test_data.iterrows():
-        sequence_tensor = sequence_to_tensor(
-                            row['incorrect_trigram_ud'].split(),
-                            len(all_tags), all_tags)
-        output = saved_rnn.evaluate(sequence_tensor)
-        guess, guess_i = category_from_output(output, categories)
+        guess = test_datapoint(rnn, row['incorrect_trigram_ud'].split(),
+                               categories, all_tags)
         nlt.append(guess)
         results.append(guess == row[GOLD_LABEL])
     test_data[MODEL_LABEL] = nlt
     test_data['result'] = results
-    output_filename = 'data/test.csv'
+    output_filename = 'data/results_predict_nlt_annotated_FCE.csv'
     print(test_data.groupby(['result']).size().reset_index(name='count'))
     test_data.to_csv(output_filename)
     return output_filename
 
 
+def predict_nlt(n_hidden, saved_model_path, train_new_model=True):
+    training_data, test_data = setup_train_test_data(
+                                'data/testing data/annotated_FCE/' +
+                                'chinese_annotated_errors.csv', 0.2,
+                                GOLD_LABEL)
+    all_tags = get_all_tags(
+                    ['data/training data/incorrect_trigram_ud_0_vocab.csv'])
+    columns = {True: True, False: False}
+    categories = list(columns.keys())
+    if train_new_model:
+        data_dict = setup_data(training_data, columns, 'incorrect_trigram_ud',
+                               GOLD_LABEL)
+        learning_rate = 0.025
+        rnn = train_rnn_model(data_dict, categories, all_tags, learning_rate,
+                              saved_model_path, 'all_losses_predict_nlt.png')
+    else:
+        rnn = RNN(len(all_tags), n_hidden, len(categories))
+        rnn.load_state_dict(torch.load(saved_model_path))
+        rnn.eval()
+    results_file = test_nlt_rnn(test_data, rnn, categories,
+                                len(all_tags), all_tags)
+    confusion_matrix(results_file, GOLD_LABEL, MODEL_LABEL,
+                     'confusion_matrix_predict_nlt.png')
+
+
 if __name__ == "__main__":
-    test = False
+    test = True
     if test:
         vocab_datasets = [
             'data/training data/globalvoices_vocabs/zhs_ud_0_vocab.csv',
             'data/training data/globalvoices_vocabs/en_ud_0_vocab.csv']
         training_datasets = [
             'data/training data/tagged_globalvoices_sentences.csv']
-        testing_dataset = 'data/testing data/annotated_FCE/' + \
-                          'chinese_annotated_errors.csv'
+        test_dataset = 'data/testing data/annotated_FCE/' + \
+                       'chinese_annotated_errors.csv'
         categories = ['en_ud', 'zhs_ud']
         n_hidden = 256
         saved_model_path = './saved_model_zhs_en_1.pth'
-        nli(vocab_datasets, training_datasets, testing_dataset,
+        nli(vocab_datasets, training_datasets, test_dataset,
             categories, n_hidden, saved_model_path, False)
     else:
         n_hidden = 128
         saved_model_path = 'saved_prediction_model.pth'
-        predict_nlt(n_hidden, saved_model_path, False)
+        predict_nlt(n_hidden, saved_model_path)
