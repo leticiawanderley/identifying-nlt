7ab52209b810cc49cd1e0471f490eb011ce12478
Leticia Wanderley <leticiafwanderley@gmail.com>
2020-08-31 12:30:47 -0600

    Create penn quadgrams vocabulary

--- process_training_data.py
+++ process_training_data.py
@@ -45,8 +45,8 @@ def create_vocabs_files(n, languages, datasets, column):
 
 def main():
     datasets = ['data/testing data/annotated_FCE/chinese_annotated_errors.csv']
-    languages = ['type_and_trigram']
-    create_vocabs_files(1, languages, datasets, 'ud')
+    languages = ['penn']
+    create_vocabs_files(1, languages, datasets, 'quadgram')
 
 
 main()
