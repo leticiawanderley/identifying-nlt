b175da4732dd61f655dded6169e4d12ec9d22ca8
Leticia Farias Wanderley <fariaswa@ualberta.ca>
2020-09-09 10:58:07 -0700

    Update sbatch script to use more data

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 #SBATCH --mail-user=fariaswa@ualberta.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-cepp
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=1024M
+#SBATCH --mem=2048M
 #SBATCH --output=slurm-%x.%j.out
 #SBATCH --error=slurm-%x.%j.err
 module load python/3.7
