f3b996ed450ced546557b33952e41d9b5c51bb5c
Leticia Wanderley <leticiafwanderley@gmail.com>
2021-05-31 14:33:05 -0600

    Implement n-gram tuning cross-validation

--- n_gram_tuning.py
+++ n_gram_tuning.py
@@ -1,6 +1,7 @@
 import kenlm
-
 import pandas as pd
+
+from statistics import mean, median
 
 
 def hyperparameter_eval(eval_dataset, languages, model_en, model_zhs):
@@ -18,12 +19,31 @@ def hyperparameter_eval(eval_dataset, languages, model_en, model_zhs):
     return correct/(len(sequences) * 2)
 
 
-def hyperparameter_tuning(eval_dataset, n):
-    arpa_template = 'data/training_data/chinese_english/_splits/{}_{}_split.arpa'
-    for i in range(2, n + 1):
-        model_en = kenlm.LanguageModel(arpa_template.format('en', str(i)))
-        model_zhs = kenlm.LanguageModel(arpa_template.format('zhs', str(i)))
-        print('{0}-gram en model'.format(model_en.order))
-        print('{0}-gram zhs model'.format(model_zhs.order))
-        print(hyperparameter_eval(eval_dataset, ['en', 'zhs'], model_en,
-                                  model_zhs))
+def hyperparameter_tuning(n):
+    arpa_template = \
+        'data/training_data/chinese_english_splits/n-gram_tuning/{}_{}_{}.arpa'
+    cv_results = {}
+    for n in range(2, n + 1):
+        cv_results[n] = []
+        for f in range(1, 6):
+            model_en = kenlm.LanguageModel(
+                arpa_template.format('en', str(f), str(n)))
+            model_zhs = kenlm.LanguageModel(
+                arpa_template.format('zhs', str(f), str(n)))
+            print('{0}-gram en model'.format(model_en.order))
+            print('{0}-gram zhs model'.format(model_zhs.order))
+            eval_dataset = \
+                'data/training_data/chinese_english_splits/n-gram_tuning/test_fold_' + \
+                str(f) + '.csv'
+            cv_results[n].append(hyperparameter_eval(eval_dataset,
+                                 ['en', 'zhs'], model_en, model_zhs))
+    tuning_results = {'n': [], 'mean': [], 'median': []}
+    for key in cv_results:
+        tuning_results['n'].append(key)
+        tuning_results['mean'].append(mean(cv_results[key]))
+        tuning_results['median'].append(median(cv_results[key]))
+    pd.DataFrame.from_dict(tuning_results).to_csv(
+        'data/tuning_results_n-gram/n-gram_tuning_results.csv')
+
+
+hyperparameter_tuning(6)
